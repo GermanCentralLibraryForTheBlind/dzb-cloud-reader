3243bb9d5f89e0b79c54a5f40e6769fd1b636994
Lars Voigt <lars.voigt@dzb.de>
2014-12-05 13:17:37 +0100

    update cloud reader

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+module.exports = function(grunt) {
+// Load Grunt tasks declared in the package.json file
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+
+    grunt.initConfig({
+
+// Grunt express - our webserver
+// https://github.com/blai/grunt-express
+        express: {
+            all: {
+                options: {
+                    bases: process.cwd(),
+                    port: 8080,
+                    livereload: true
+                }
+            }
+        },
+
+// grunt-watch will monitor the projects files
+// https://github.com/gruntjs/grunt-contrib-watch
+        watch: {
+            all: {
+                files: '**/*.*',
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+// Creates the `server` task
+    grunt.registerTask('default', ['express','watch']);
+};
