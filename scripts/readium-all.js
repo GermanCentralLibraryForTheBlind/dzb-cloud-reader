68a9d0f9f162a1adede7b3a5f2c26c40299ad580
AndreasJRudolph <andreas.rudolph@dzb.de>
2015-04-21 13:42:07 +0200

    - fixed layout bug: right navbartoo close to the edge

--- scripts/readium-all.js
+++ scripts/readium-all.js
@@ -15350,7 +15350,7 @@ define('analytics/Analytics',[],function(){
 	}
 });
 
-define('text!viewer-version',[],function () { return '{"version":"0.17.0","chromeVersion":"2.17.0","sha":"8d984a7bab6e3e1e003ba4b24e5023a36e0e67fe","tag":"0.17.0-375-g8d984a7","clean":false,"release":false,"timestamp":1429616352814}';});
+define('text!viewer-version',[],function () { return '{"version":"0.17.0","chromeVersion":"2.17.0","sha":"8d984a7bab6e3e1e003ba4b24e5023a36e0e67fe","tag":"0.17.0-375-g8d984a7","clean":false,"release":false,"timestamp":1429616502781}';});
 
 //     Underscore.js 1.4.4
 //     http://underscorejs.org
